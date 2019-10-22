2ca10988475accd7a92cf4af6e44c43199a54ba9
collin.lesko <lesko1cj@alma.edu>
2019-10-22 08:36:42 -0400

    removed db comments

--- src/main/java/works/weave/socks/cart/controllers/HealthCheckController.java
+++ src/main/java/works/weave/socks/cart/controllers/HealthCheckController.java
@@ -28,8 +28,7 @@ public class HealthCheckController {
        List<HealthCheck> healthChecks = new ArrayList<HealthCheck>();
        Date dateNow = Calendar.getInstance().getTime();
 
-       HealthCheck app = new HealthCheck("carts", "OK", dateNow);
-       /*
+       HealthCheck app = new HealthCheck("carts", "OK", dateNow);      
        HealthCheck database = new HealthCheck("carts-db", "OK", dateNow);
 
        
@@ -38,10 +37,10 @@ public class HealthCheckController {
        } catch (Exception e) {
           database.setStatus("err");
        }
-       */
+       
 
        healthChecks.add(app);
-       // healthChecks.add(database);
+       healthChecks.add(database);
 
        map.put("health", healthChecks);
        return map;
